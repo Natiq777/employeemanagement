f232bd4fad20c47f11e1857a575295958a12c72e
n.abdulahadov <n.abdulahadov@EGPC061>
2023-11-27 17:14:41 +0400

    migration update

--- Infrastructure/Migrations/20231127120437_EM-v1.cs
+++ Infrastructure/Migrations/20231127120437_EM-v1.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class EmployeManagementv1 : Migration
+    public partial class EMv1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
